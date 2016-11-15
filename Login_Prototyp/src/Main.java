0378d62f295603fdde5c617161cc9816d090ef58
VatrasCell <VatrasCell@users.noreply.github.com>
2016-11-15 16:01:01 +0100

    Add files via upload
    Login v1

---
+++ Login_Prototyp/src/Main.java
@@ -0,0 +1,133 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+
+public class Main {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+		
+		JLabel lblLogin = new JLabel("Login");
+		GridBagConstraints gbc_lblLogin = new GridBagConstraints();
+		gbc_lblLogin.insets = new Insets(0, 0, 5, 5);
+		gbc_lblLogin.gridx = 5;
+		gbc_lblLogin.gridy = 0;
+		frame.getContentPane().add(lblLogin, gbc_lblLogin);
+		
+		JLabel lblName = new JLabel("Name");
+		GridBagConstraints gbc_lblName = new GridBagConstraints();
+		gbc_lblName.insets = new Insets(0, 0, 5, 5);
+		gbc_lblName.anchor = GridBagConstraints.EAST;
+		gbc_lblName.gridx = 4;
+		gbc_lblName.gridy = 2;
+		frame.getContentPane().add(lblName, gbc_lblName);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 5;
+		gbc_textField.gridy = 2;
+		frame.getContentPane().add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblPasswort = new JLabel("Passwort");
+		GridBagConstraints gbc_lblPasswort = new GridBagConstraints();
+		gbc_lblPasswort.anchor = GridBagConstraints.EAST;
+		gbc_lblPasswort.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPasswort.gridx = 4;
+		gbc_lblPasswort.gridy = 3;
+		frame.getContentPane().add(lblPasswort, gbc_lblPasswort);
+		
+		passwordField = new JPasswordField();
+		GridBagConstraints gbc_passwordField = new GridBagConstraints();
+		gbc_passwordField.insets = new Insets(0, 0, 5, 5);
+		gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_passwordField.gridx = 5;
+		gbc_passwordField.gridy = 3;
+		frame.getContentPane().add(passwordField, gbc_passwordField);
+		
+		JButton btnLogin = new JButton("Login");
+		GridBagConstraints gbc_btnLogin = new GridBagConstraints();
+		gbc_btnLogin.insets = new Insets(0, 0, 0, 5);
+		gbc_btnLogin.gridx = 4;
+		gbc_btnLogin.gridy = 5;
+		frame.getContentPane().add(btnLogin, gbc_btnLogin);
+		
+		btnLogin.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if(!textField.getText().equals("") && !passwordField.getPassword().equals("")) {
+					System.out.println(String.valueOf(passwordField.getPassword()));
+
+					final String strToEncrypt = String.valueOf(passwordField.getPassword());
+	                final String strPssword = "encryptor key";
+	                AES.setKey(strPssword);
+	               
+	                AES.encrypt(strToEncrypt.trim());
+	                
+	                System.out.println("String to Encrypt: " + strToEncrypt); 
+	                System.out.println("Encrypted: " + AES.getEncryptedString());
+	           
+	                final String strToDecrypt =  AES.getEncryptedString();
+	                AES.decrypt(strToDecrypt.trim());
+	               
+	                System.out.println("String To Decrypt : " + strToDecrypt);
+	                System.out.println("Decrypted : " + AES.getDecryptedString());
+
+				}
+			}
+		});
+	}
+
+}
